bf9f4edeaa7ba46f8a1e70c07ad5cfee66d1049a
rmys6 <37138243+rmys6@users.noreply.github.com>
2020-05-24 20:04:18 +0300

    --> ng-select added to External Cost and PGK Cost. --> Button added to User Table.

--- src/app/views/pgk-costs-parameter/pgk-costs-parameter.component.ts
+++ src/app/views/pgk-costs-parameter/pgk-costs-parameter.component.ts
@@ -11,6 +11,26 @@ export class PgkCostsParameterComponent implements OnInit {
 
   constructor() { }
 
+  public PGKCostDefinitionList: Array<IOption> = [
+    {label: 'Item 1 from Cost Center', value: 'Item1'},
+    {label: 'Item 2 from Cost Center', value: 'Item2'},
+  ];
+
+  public StatusList: Array<IOption> = [
+    {label: 'Status 1', value: 'Item1'},
+    {label: 'Status 2', value: 'Item2'},
+  ];
+
+  public PlanningPeriods: Array<IOption> = [
+    {label: 'EA1', value: 'Item1'},
+    {label: 'EA2', value: 'Item2'},
+  ];
+
+  public CurrencyList: Array<IOption> = [
+    {label: 'EUR', value: 'Item1'},
+    {label: 'TL', value: 'Item2'},
+  ];
+
   ngOnInit(): void {
   }
 
