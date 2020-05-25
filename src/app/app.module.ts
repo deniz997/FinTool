dc7502ee3e2969cce6a794a9807f8e23c9e59ac4
MinelKupcu <minelkupcu@gmail.com>
2020-05-25 14:55:30 +0200

    add year select in sla item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,6 +47,7 @@ import { DepartmentTravelRateComponent } from './views/department-travel-rate/de
 import {SelectModule} from 'ng-select';
 import {PaginationModule} from 'ngx-bootstrap/pagination';
 import {FormsModule} from '@angular/forms';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   imports: [
@@ -65,6 +66,7 @@ import {FormsModule} from '@angular/forms';
     SelectModule,
     PaginationModule.forRoot(),
     FormsModule,
+    BsDatepickerModule,
   ],
   declarations: [
     AppComponent,
