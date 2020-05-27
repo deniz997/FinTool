410dfd00daf1060a137021e45e470720b8943299
MinelKupcu <minelkupcu@gmail.com>
2020-05-27 15:14:58 +0200

    fix date range picker

--- src/app/views/sla-item-parameter/sla-item-parameter.component.ts
+++ src/app/views/sla-item-parameter/sla-item-parameter.component.ts
@@ -5,31 +5,7 @@ import {NgbDate, NgbCalendar, NgbDateParserFormatter} from '@ng-bootstrap/ng-boo
 @Component({
   selector: 'app-sla-item-parameter',
   templateUrl: './sla-item-parameter.component.html',
-  styleUrls: ['./sla-item-parameter.component.css'][`
-    .form-group.hidden {
-      width: 0;
-      margin: 0;
-      border: none;
-      padding: 0;
-    }
-    .custom-day {
-      text-align: center;
-      padding: 0.185rem 0.25rem;
-      display: inline-block;
-      height: 2rem;
-      width: 2rem;
-    }
-    .custom-day.focused {
-      background-color: #e6e6e6;
-    }
-    .custom-day.range, .custom-day:hover {
-      background-color: rgb(2, 117, 216);
-      color: white;
-    }
-    .custom-day.faded {
-      background-color: rgba(2, 117, 216, 0.5);
-    }
-  `]
+  styleUrls: ['./sla-item-parameter.component.css']
 })
 export class SlaItemParameterComponent implements OnInit {
 
