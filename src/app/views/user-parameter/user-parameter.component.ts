d16628ede2864656b2c48e30cdf94afedec1e5f5
MinelKupcu <minelkupcu@gmail.com>
2020-06-01 10:51:14 +0200

    change table in user-parameter fix pagination

--- src/app/views/user-parameter/user-parameter.component.ts
+++ src/app/views/user-parameter/user-parameter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IOption} from 'ng-select';
 
 @Component({
   selector: 'app-user-parameter',
@@ -8,6 +9,29 @@ import { Component, OnInit } from '@angular/core';
 export class UserParameterComponent implements OnInit {
 
   constructor() { }
+
+  rightTableFirstCellHeader = 'Who is Who ID';
+
+  rightTableHeaders = [
+    'Cost Center',
+    'Name',
+    'User Typ',
+    'FTE Capacity',
+    'Type of Employment',
+    'Assignment Status',
+    'Current Level',
+    'Hiring Time Frame',
+  ];
+
+  rightTableData = [
+    ['ID_001', 'Cost Center 1', 'Yavuz Saka', 'User Typ 1', 'FTE Capacity 1', 'Type 1', 'Assignment Status 1', 'Current Level 1', 'Hiring Time Frame 1'],
+    ['ID_002', 'Cost Center 2', 'Josef Aksac', 'User Typ 2', 'FTE Capacity 2', 'Type 2', 'Assignment Status 2', 'Current Level 2', 'Hiring Time Frame 2'],
+  ];
+
+  currentPage: number = 1;
+  totalItems: number = 60;
+  itemPerPage: number = 5;
+  maxSize: number = 7;
 
   ngOnInit(): void {
   }
