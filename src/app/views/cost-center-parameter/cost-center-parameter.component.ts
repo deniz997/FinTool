5a36548963a3a9b158ab0662e56c1779afb13855
MinelKupcu <minelkupcu@gmail.com>
2020-06-03 22:00:24 +0200

    fix cost center

--- src/app/views/cost-center-parameter/cost-center-parameter.component.ts
+++ src/app/views/cost-center-parameter/cost-center-parameter.component.ts
@@ -10,22 +10,38 @@ import {ngbAutoClose} from '@ng-bootstrap/ng-bootstrap/util/autoclose';
 export class CostCenterParameterComponent implements OnInit {
 
   constructor() {
+    this.data = [
+      {
+        CostCenterID: 'Cost Center ID 1',
+        CostCenterName: 'Cost Center Name 1',
+        CostCenterTyp: 'Typ 1'
+      },
+      {
+        CostCenterID: 'Cost Center ID 2',
+        CostCenterName: 'Cost Center Name 2',
+        CostCenterTyp: 'Typ 1'
+      },
+      {
+        CostCenterID: 'Cost Center ID 3',
+        CostCenterName: 'Cost Center Name 3',
+        CostCenterTyp: 'Typ 2'
+      },
+    ];
+    this.setClickedRow = function (index) {
+      this.selectedRow = index;
+    };
   }
 
-  currentCostCenter: any;
-
-  rightTableFirstCellHeader = 'Cost Center ID';
-
-  rightTableHeaders = [
+  tableHeaders = [
+    'Cost Center ID',
     'Cost Center Name',
     'Cost Center Typ',
   ];
 
-  rightTableData = [
-    ['Cost Center ID 1', 'Cost Center Name 1', 'Typ 1'],
-    ['Cost Center ID 2', 'Cost Center Name 2', 'Typ 1'],
-    ['Cost Center ID 3', 'Cost Center Name 3', 'Typ 2'],
-  ];
+  data: [{CostCenterID: string; CostCenterName: string; CostCenterTyp: string}, {CostCenterID: string; CostCenterName: string; CostCenterTyp: string}, {CostCenterID: string; CostCenterName: string; CostCenterTyp: string}];
+
+  selectedRow: Number;
+  setClickedRow: Function;
 
   showAdd = false;
   showUpdate = false;
@@ -39,9 +55,6 @@ export class CostCenterParameterComponent implements OnInit {
     {label: 'Typ 1', value: 'Typ1'},
     {label: 'Typ 2', value: 'Typ2'},
   ];
-
-  showPicker: boolean;
-  clickedRow = [];
 
   showAddCard() {
     this.showAdd = !this.showAdd;
@@ -68,38 +81,5 @@ export class CostCenterParameterComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.clickedRow.push('');
-    this.rightTableHeaders.forEach(_ => {
-      this.clickedRow.push('');
-    });
-  }
-
-  saveClickedRow(i: number) {
-    this.emptyClickedRow();
-    this.rightTableData[i].forEach(value => {
-      this.clickedRow.push(value);
-    });
-  }
-
-  onTableRowClick(i: number) {
-    this.currentCostCenter = this.rightTableData[i][0];
-    this.saveClickedRow(i);
-    this.showUpdateCard();
-  }
-
-  emptyClickedRow() {
-    this.clickedRow = [];
-  }
-
-  getPlaceHolderForUpdateFields(i: number): string {
-    if (this.currentCostCenter === this.getNextCostCenterID() && i === 0) {
-      return 'Cost Center ID 1';
-    } else {
-      return 'Typ 1';
-    }
-  }
-
-  getNextCostCenterID(): string {
-    return 'Cost Center ID 1';
   }
 }
