a1b55b56503f12e0f032aa2574195b6817e0660f
Alp A <38848833+Umptor@users.noreply.github.com>
2020-06-04 18:51:33 +0300

    update external headcount to internal headcount

--- src/app/views/external-headcount/external-headcount.component.ts
+++ src/app/views/external-headcount/external-headcount.component.ts
@@ -2,7 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
 import {IOption} from 'ng-select';
 import PerfectScrollbar from 'perfect-scrollbar';
-import {timer} from 'rxjs';
+import {Observable, timer} from 'rxjs';
 
 @Component({
   selector: 'app-external-headcount',
@@ -11,15 +11,6 @@ import {timer} from 'rxjs';
 })
 export class ExternalHeadcountComponent implements OnInit {
   constructor(@Inject(DOCUMENT) document) { }
-
-  // costCategories: Array<String> = ['Primary Cost', 'Project Cost Indirect Chargeable'];
-  public costCenters: Array<IOption> = [
-    {label: 'Regional Rollout P', value: 'P'},
-    {label: 'Regional Rollout A', value: 'A'},
-    {label: 'Regional Rollout B', value: 'B'},
-    {label: 'Regional Rollout C', value: 'C'},
-    {label: 'Total Extern Headcount Cost from Capa Plan', value: 'Capa Plan'}
-  ];
 
   public cardValues = [
 
@@ -48,32 +39,24 @@ export class ExternalHeadcountComponent implements OnInit {
     '2026',
   ];
 
-  public regionalRolloutPValues = [
-    2, 3, 5, 7, 6, 5, 5, 5, 5, 4, 3, 2
+  tableData = [
+    ['Regional Rollout P', '2', '3', '5', '7', '6', '5', '5', '5', '5', '4', '3', '2'],
+    ['Regional Rollout A', '8', '8', '8', '8', '8', '8', '10', '11', '12', '13', '14', '15'],
+    ['Regional Rollout B', '9', '9', '9', '7', '7', '7', '7', '8', '8', '8', '8', '8'],
+    ['Regional Rollout C', '10', '11', '12', '12', '12', '12', '12', '12', '12', '12', '12', '12'],
+    ['Total Extern Headcount Cost per Class'],
+    ['Total Extern Headcount Cost from Capa Plan', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
   ];
 
-  public regionalRolloutAValues = [
-    8, 8, 8, 8, 8, 8, 10, 11, 12, 13, 14, 15
-  ];
-
-  public regionalRolloutBValues = [
-    9, 9, 9, 7, 7, 7, 7, 8, 8, 8, 8, 8
-  ];
-
-  public regionalRolloutCValues = [
-    10, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12
-  ];
-
-  public totalExternHeadCountCostPerClass = [];
-
-  public totalExternHeadCountCostFromCapaPlan = [
-    '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'
-  ];
   months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  currentClass: any;
+
+  totalExternHeadcountCostPerClassIndex = 4;
+  showInputField: boolean;
+  selectedRowNumber: number;
+  validSelectedRowNumber: boolean = false;
 
   ngOnInit(): void {
-    timer(150).subscribe(_ => {
+    this.sleep(150).subscribe(_ => {
       this.tableResized();
     });
     this.fillTable();
@@ -81,29 +64,27 @@ export class ExternalHeadcountComponent implements OnInit {
   }
 
   fillCards() {
+    for (let i = 0; i < 4; i++) {
+      this.cardValues.push(this.getSumOfRow(i));
+    }
+  }
+  getSumOfRow(row: number) {
     let total = 0;
-    this.regionalRolloutPValues.forEach(value => total += value);
-    this.cardValues.push(total);
-    total = 0;
-    this.regionalRolloutAValues.forEach(value => total += value);
-    this.cardValues.push(total);
-    total = 0;
-    this.regionalRolloutBValues.forEach(value => total += value);
-    this.cardValues.push(total);
-    total = 0;
-    this.regionalRolloutCValues.forEach(value => total += value);
-    this.cardValues.push(total);
+    for (let i = 1; i <= 12; i++) {
+      total += parseInt(this.tableData[row][i], 10);
+    }
+    return total;
   }
 
   fillTable() {
-    this.regionalRolloutCValues.forEach((value, index) => {
-      this.totalExternHeadCountCostPerClass.push(
-        50 * this.regionalRolloutPValues[index] +
-        45 * this.regionalRolloutAValues[index] +
-        40 * this.regionalRolloutBValues[index] +
-        35 * this.regionalRolloutCValues[index]
+    for (let i = 1; i <= 12; i++) {
+      this.tableData[this.totalExternHeadcountCostPerClassIndex].push(
+        (50 * parseInt(this.tableData[0][i], 10) +
+        45 * parseInt(this.tableData[1][i], 10) +
+        40 * parseInt(this.tableData[2][i], 10) +
+        35 * parseInt(this.tableData[3][i], 10)) + ''
       );
-    });
+    }
   }
 
   showYear(year: string, event) {
@@ -122,12 +103,8 @@ export class ExternalHeadcountComponent implements OnInit {
   }
 
   tableResized() {
-    const table = document.getElementById('internalHeadcountTable');
     const ps = new PerfectScrollbar('#yearScrollbar');
-    const tableHeight = table.offsetHeight;
-    const yearHeaderHeight = document.getElementById('yearHeader').offsetHeight;
-    const newHeight = `${(tableHeight - yearHeaderHeight - 6).toString()}px`;
-    document.getElementById('yearScrollbar').style.height = newHeight;
+    document.getElementById('yearScrollbar').style.height = this.getYearScrollbarNewHeight();
     ps.update();
   }
 
@@ -137,6 +114,76 @@ export class ExternalHeadcountComponent implements OnInit {
     const value = idAttr.nodeValue;
     return document.getElementById(value);
 
+  }
+
+
+  sleep(ms: number): Observable<number> {
+    return timer(ms);
+  }
+
+  getYearScrollbarNewHeight(): string {
+    const table = document.getElementById('internalHeadcountTable');
+    const tableHeight = table.offsetHeight;
+    const yearHeaderHeight = document.getElementById('yearHeader').offsetHeight;
+    return `${(tableHeight - yearHeaderHeight - 6).toString()}px`;
+  }
+
+  closeInputCard() {
+    this.showInputField = false;
+  }
+
+  openInputCard() {
+    this.showInputField = true;
+  }
+
+  saveClickedRow(i: number) {
+    this.selectedRowNumber = i;
+    this.updateValidSelectedRowNumber();
+  }
+
+  onTableRowClick(i: number) {
+    this.saveClickedRow(i);
+    this.closeInputCard();
+    this.updateSizeOnEditButtonAppearanceChange();
+  }
+
+  updateSizeOnEditButtonAppearanceChange() {
+    this.tableResized();
+    this.sleep(25).subscribe(() => {
+      if (this.getYearScrollbarNewHeight() !== document.getElementById('yearScrollbar').style.height) {
+        this.sleep(25).subscribe(() => {
+          this.updateSizeOnEditButtonAppearanceChange();
+        });
+      }
+    });
+  }
+
+
+  openEditCard() {
+    this.openInputCard();
+  }
+
+  clearClickedRow() {
+    this.saveClickedRow(-1);
+  }
+
+  updateValidSelectedRowNumber() {
+    this.validSelectedRowNumber = !(this.selectedRowNumber >= this.tableData.length || this.selectedRowNumber < 0)
+      && this.selectedRowNumber !== this.totalExternHeadcountCostPerClassIndex;
+  }
+
+  getDefaultValueForInput(columnNumber: number) {
+    return this.tableData[this.selectedRowNumber][columnNumber + 1];
+  }
+
+  getPlaceHolderForInputFields(columnNumber: number): string {
+    return 'Ex. 4';
+  }
+
+  submit() {
+    this.closeInputCard();
+    this.clearClickedRow();
+    this.updateSizeOnEditButtonAppearanceChange();
   }
 
 }
