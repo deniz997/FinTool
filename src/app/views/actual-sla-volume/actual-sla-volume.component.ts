51622738b53023fff300dc62cc2c6a37b6aef384
Alp A <38848833+Umptor@users.noreply.github.com>
2020-06-04 15:46:52 +0300

    rename rightTable to table in actual sla volume

--- src/app/views/actual-sla-volume/actual-sla-volume.component.ts
+++ src/app/views/actual-sla-volume/actual-sla-volume.component.ts
@@ -10,7 +10,7 @@ export class ActualSlaVolumeComponent implements OnInit {
 
   constructor() { }
 
-  rightTableHeaders = [
+  tableHeaders = [
     'SLA No',
     'SLA Name',
     'Template Name',
@@ -32,7 +32,7 @@ export class ActualSlaVolumeComponent implements OnInit {
     'SLA Consumption in %'
   ];
 
-  rightTableData = [
+  tableData = [
     ['SLA20_ITTQT_008', 'Mercedes Me Connect Aftersales Rollout 5', 'MeConnectA', 'RO', '01.01.2020', '31.12.2020', '2.420', '617.100', '185', '740', '9', '46', '-', '-', '194', '70.000', '946.002', '251.554', '30%'],
     ['SLA20_ITTQT_009', 'Mercedes Me Connect 2', 'MeConnectS', 'RO', '01.01.2020', '31.12.2020', '2.420', '617.100', '168', '504', '24', '120', '-', '-', '192', '70.000', '967.516', '248.241', '25%'],
     ['SLA20_ITTQT_012', 'Mercedes me connect 1', 'MeConnectS', 'RO', '01.01.2020', '31.12.2020', '1.889', '481.695', '135', '405', '20', '100', '-', '-', '155', '70.000', '780.440', '451.684', '45%'],
@@ -51,14 +51,14 @@ export class ActualSlaVolumeComponent implements OnInit {
   ngOnInit(): void {}
 
   isInputDisabled(columnNumber: number): boolean {
-    if (this.selectedRowNumber >= this.rightTableData.length) {
+    if (this.selectedRowNumber >= this.tableData.length) {
       return true;
     }
-    return this.rightTableData[this.selectedRowNumber][columnNumber] === '-';
+    return this.tableData[this.selectedRowNumber][columnNumber] === '-';
   }
 
   getPlaceHolderForInputFields(columnNumber: number): string {
-    if (this.selectedRowNumber >= this.rightTableData.length && columnNumber === 0) {
+    if (this.selectedRowNumber >= this.tableData.length && columnNumber === 0) {
       return this.getNextSLAName();
     } else {
       return 'Ex. 4';
