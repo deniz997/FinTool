642ae1f649d94b0449727cdba212bc4887964545
MinelKupcu <minelkupcu@gmail.com>
2020-06-04 10:12:07 +0200

    fix sla item

--- src/app/views/sla-item-parameter/sla-item-parameter.component.ts
+++ src/app/views/sla-item-parameter/sla-item-parameter.component.ts
@@ -17,18 +17,32 @@ export class SlaItemParameterComponent implements OnInit {
 
     this.data = [
       {
-        Year: '0000',
-        ValidFrom: '00/00',
-        ValidTo: '00/00',
-        MandayTyp: 'a',
-        MandayRate: 'b',
-        TravelTyp: 'c',
-        TravelDayTyp: 'd',
-        TravelRate: 'e',
-        TicketTyp: 'f',
-        TicketRate: 'g',
-        FixCostsTyp: 'h',
-        FixCostsRate: 'i'
+        Year: '2020',
+        ValidFrom: '2020/01/01',
+        ValidTo: '2020/10/01',
+        MandayTyp: 'Manday Typ 1',
+        MandayRate: 'Manday Rate 1',
+        TravelTyp: 'Travel Typ 1',
+        TravelDayTyp: 'Travel Day Typ 1',
+        TravelRate: 'Travel Rate 2',
+        TicketTyp: 'Ticket Typ 2',
+        TicketRate: 'Ticket Rate 2',
+        FixCostsTyp: 'Fix Costs Typ 1',
+        FixCostsRate: 'Fix Costs Rate 2'
+      },
+      {
+        Year: '2020',
+        ValidFrom: '2020/01/01',
+        ValidTo: '2020/12/01',
+        MandayTyp: 'Manday Typ 2',
+        MandayRate: 'Manday Rate 2',
+        TravelTyp: 'Travel Typ 2',
+        TravelDayTyp: 'Travel Day Typ 2',
+        TravelRate: 'Travel Rate 1',
+        TicketTyp: 'Ticket Typ 1',
+        TicketRate: 'Ticket Rate 1',
+        FixCostsTyp: 'Fix Costs Typ 2',
+        FixCostsRate: 'Fix Costs Rate 1'
       },
     ];
     this.setClickedRow = function (index) {
@@ -74,48 +88,48 @@ export class SlaItemParameterComponent implements OnInit {
   toDate: NgbDate | null;
 
   public mandayTyp: Array<IOption> = [
-    {label: 'Typ 1', value: 'Typ1'},
-    {label: 'Typ 2', value: 'Typ2'},
+    {label: 'Manday Typ 1', value: 'MandayTyp1'},
+    {label: 'Manday Typ 2', value: 'MandayTyp2'},
   ];
 
   public mandayRate: Array<IOption> = [
-    {label: 'Typ 1', value: 'Typ1'},
-    {label: 'Typ 2', value: 'Typ2'},
+    {label: 'Manday Rate 1', value: 'MandayRate1'},
+    {label: 'Manday Rate 2', value: 'MandayRate2'},
   ];
 
   public travelTyp: Array<IOption> = [
-    {label: 'Typ 1', value: 'Typ1'},
-    {label: 'Typ 2', value: 'Typ2'},
+    {label: 'Travel Typ 1', value: 'TravelTyp1'},
+    {label: 'Travel Typ 2', value: 'TravelTyp2'},
   ];
 
   public travelDayTyp: Array<IOption> = [
-    {label: 'Typ 1', value: 'Typ1'},
-    {label: 'Typ 2', value: 'Typ2'},
+    {label: 'Travel Day Typ 1', value: 'TravelDayTyp1'},
+    {label: 'Travel Day Typ 2', value: 'TravelDayTyp2'},
   ];
 
   public travelRate: Array<IOption> = [
-    {label: 'Typ 1', value: 'Typ1'},
-    {label: 'Typ 2', value: 'Typ2'},
+    {label: 'Travel Rate 1', value: 'TravelRate1'},
+    {label: 'Travel Rate 2', value: 'TravelRate2'},
   ];
 
   public ticketTyp: Array<IOption> = [
-    {label: 'Typ 1', value: 'Typ1'},
-    {label: 'Typ 2', value: 'Typ2'},
+    {label: 'Ticket Typ 1', value: 'TicketTyp1'},
+    {label: 'Ticket Typ 2', value: 'TicketTyp2'},
   ];
 
   public ticketRate: Array<IOption> = [
-    {label: 'Typ 1', value: 'Typ1'},
-    {label: 'Typ 2', value: 'Typ2'},
+    {label: 'Ticket Rate 1', value: 'TicketRate1'},
+    {label: 'Ticket Rate 2', value: 'TicketRate2'},
   ];
 
   public fixCostsTyp: Array<IOption> = [
-    {label: 'Typ 1', value: 'Typ1'},
-    {label: 'Typ 2', value: 'Typ2'},
+    {label: 'Fix Costs Typ 1', value: 'FixCostsTyp1'},
+    {label: 'Fix Costs Typ 2', value: 'FixCostsTyp2'},
   ];
 
   public fixCostsRate: Array<IOption> = [
-    {label: 'Typ 1', value: 'Typ1'},
-    {label: 'Typ 2', value: 'Typ2'},
+    {label: 'Fix Costs Rate 1', value: 'FixCostsRate1'},
+    {label: 'Fix Costs Rate 2', value: 'FixCostsRate2'},
   ];
 
   setPage(pageNo: number): void {
