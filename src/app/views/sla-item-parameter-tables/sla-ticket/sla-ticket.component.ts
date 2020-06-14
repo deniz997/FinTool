8beb2ef4e7af91b9d75cc90b1c839e464c22fe3e
MinelKupcu <minelkupcu@gmail.com>
2020-06-14 19:27:46 +0200

    save sla-ticket component

---
+++ src/app/views/sla-item-parameter-tables/sla-ticket/sla-ticket.component.ts
@@ -0,0 +1,156 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbCalendar, NgbDate, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
+import {IOption} from 'ng-select';
+
+@Component({
+  selector: 'app-sla-ticket',
+  templateUrl: './sla-ticket.component.html',
+  styleUrls: ['./sla-ticket.component.css']
+})
+export class SlaTicketComponent implements OnInit {
+
+  private data = [];
+
+  constructor(private calendar: NgbCalendar, public formatter: NgbDateParserFormatter) {
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+
+    this.data = [
+      {
+        Year: '2020',
+        ValidFrom: '2020/01/01',
+        ValidTo: '2020/10/01',
+        TicketTyp: 'Ticket Typ 2',
+        TicketRate: 'Ticket Rate 2'
+      },
+      {
+        Year: '2020',
+        ValidFrom: '2020/01/01',
+        ValidTo: '2020/12/01',
+        TicketTyp: 'Ticket Typ 1',
+        TicketRate: 'Ticket Rate 1'
+      },
+    ];
+    this.setClickedRow = function (index) {
+      this.selectedRow = index;
+    };
+  }
+
+  tableSubHeaders = [
+    'Year',
+    'Valid From',
+    'Valid To',
+    'Ticket Typ',
+    'Ticket Rate',
+  ];
+
+  selectedRow: string;
+  setClickedRow: Function;
+  selectedRowNumber: number;
+  validSelectedRowNumber: boolean = false;
+
+  showAdd = false;
+  showUpdate = false;
+
+  totalItems: number = 60;
+  currentPage: number   = 1;
+  itemPerPage: number = 5;
+  maxSize: number = 7;
+
+  hoveredDate: NgbDate | null = null;
+
+  fromDate: NgbDate | null;
+  toDate: NgbDate | null;
+
+  public year: Array<IOption> = [
+    {label: '2020', value: 'Year1'},
+    {label: '2019', value: 'Year2'},
+    {label: '2018', value: 'Year3'},
+    {label: '2017', value: 'Year4'},
+    {label: '2016', value: 'Year5'},
+    {label: '2015', value: 'Year6'},
+  ];
+  public ticketTyp: Array<IOption> = [
+    {label: 'Ticket Typ 1', value: 'TicketTyp1'},
+    {label: 'Ticket Typ 2', value: 'TicketTyp2'},
+  ];
+
+  public ticketRate: Array<IOption> = [
+    {label: 'Ticket Rate 1', value: 'TicketRate1'},
+    {label: 'Ticket Rate 2', value: 'TicketRate2'},
+  ];
+
+  ngOnInit(): void {
+  }
+
+  saveClickedRow(i: number) {
+    this.selectedRowNumber = i;
+    this.updateValidSelectedRowNumber();
+  }
+
+  onTableRowClick(i: number) {
+    this.saveClickedRow(i);
+    this.closeUpdateCard();
+  }
+
+  updateValidSelectedRowNumber() {
+    this.validSelectedRowNumber = !(this.selectedRowNumber >= this.data.length || this.selectedRowNumber < 0);
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
+  }
+
+  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+    const parsed = this.formatter.parse(input);
+    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+  }
+
+  showAddCard() {
+    this.showAdd = !this.showAdd;
+  }
+
+  closeAddCard() {
+    this.showAdd = false;
+  }
+
+  showUpdateCard() {
+    this.showUpdate = !this.showUpdate;
+  }
+
+  closeUpdateCard() {
+    this.showUpdate = false;
+  }
+
+  saveAdd() {
+    this.closeAddCard();
+  }
+
+  saveUpdate() {
+    this.closeUpdateCard();
+  }
+
+  SetSelectedRow(): void {
+    this.selectedRowNumber = null;
+  }
+
+}
